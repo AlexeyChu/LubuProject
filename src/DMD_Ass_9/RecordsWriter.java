e4ca472bbb96c26dda99446f7fb7e7468dafb206
AlexeyChu <alexmail_87@mail.ru>
2015-10-18 01:57:32 +0300

    Lubuntu Projects

--- src/DMD_Ass_9/RecordsWriter.java
+++ src/DMD_Ass_9/RecordsWriter.java
@@ -1,7 +1,24 @@
 package DMD_Ass_9;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 /**
  * Created by alex on 18.10.15.
  */
 public class RecordsWriter {
 
+    public static void main(String[] args) {
+
+    }
+
+    public static void write(File file
+    ) {
+        //BufferedReader reader = new BufferedReader((new InputStreamReader()))
+        PrintWriter writer = new PrintWriter()
+
+    }
+
+}
